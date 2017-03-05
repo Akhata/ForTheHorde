172b19d2a7ea3f8beee5b18f64620587b938f10f
Akhata <isa.mrodrigues@icloud.com>
2017-03-05 19:55:49 -0300

    Commit #01

---
+++ src/com/exercicio/A.java
@@ -0,0 +1,22 @@
+package com.exercicio;
+
+public class A {
+
+	private int a1;
+	private int a2;
+	
+	public int getA1() {
+		return a1;
+	}
+	public void setA1(int a1) {
+		this.a1 = a1;
+	}
+	public int getA2() {
+		return a2;
+	}
+	public void setA2(int a2) {
+		this.a2 = a2;
+	}	
+	
+	
+}
