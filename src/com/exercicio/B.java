f258194f66be588df479cd1d620230a55e6f63a5
Akhata <isa.mrodrigues@icloud.com>
2017-03-05 21:06:49 -0300

    Commit #09

--- src/com/exercicio/B.java
+++ src/com/exercicio/B.java
@@ -4,6 +4,7 @@ public class B {
 	
 	private int b1;
 	private float b2;
+	private String b4;
 	
 	public int getB1() {
 		return b1;
@@ -17,6 +18,13 @@ public class B {
 	public void setB2(float b2) {
 		this.b2 = b2;
 	}
+	public String getB4() {
+		return b4;
+	}
+	public void setB4(String b4) {
+		this.b4 = b4;
+	}
+	
 	
 	//Foi mal, esqueci da tag
 }
